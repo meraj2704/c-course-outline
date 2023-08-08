9d15dab32e2ebb70af09575b3421f49a1fbf3dfb
IJE2704 <mh1669101@gmail.com>
2023-08-08 11:06:49 +0600

    c course outline

---
+++ dom.js
@@ -0,0 +1,131 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById('start-btn');
+    startButton.addEventListener("click", function () {
+        window.location.href = "day1.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day1btn = document.getElementById('d1-nxt-btn');
+    day1btn.addEventListener("click", function () {
+        window.location.href = "day2.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day2btn = document.getElementById('d2-nxt-btn');
+    day2btn.addEventListener("click", function () {
+        window.location.href = "day3.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day3btn = document.getElementById('d3-nxt-btn');
+    day3btn.addEventListener("click", function () {
+        window.location.href = "day4.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day4btn = document.getElementById('d4-nxt-btn');
+    day4btn.addEventListener("click", function () {
+        window.location.href = "day5.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day5btn = document.getElementById('d5-nxt-btn');
+    day5btn.addEventListener("click", function () {
+        window.location.href = "day6.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day6btn = document.getElementById('d6-nxt-btn');
+    day6btn.addEventListener("click", function () {
+        window.location.href = "day7.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day7btn = document.getElementById('d7-nxt-btn');
+    day7btn.addEventListener("click", function () {
+        window.location.href = "day8.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day8btn = document.getElementById('d8-nxt-btn');
+    day8btn.addEventListener("click", function () {
+        window.location.href = "day9.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day9btn = document.getElementById('d9-nxt-btn');
+    day9btn.addEventListener("click", function () {
+        window.location.href = "day10.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day10btn = document.getElementById('d10-nxt-btn');
+    day10btn.addEventListener("click", function () {
+        window.location.href = "day11.html";
+    });
+});
+
+
+// prev button is hrere
+
+document.addEventListener("DOMContentLoaded", function () {
+    const day2Prevbtn = document.getElementById('d2-prev-btn');
+    day2Prevbtn.addEventListener("click", function () {
+        window.location.href = "day1.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day3Prevbtn = document.getElementById('d3-prev-btn');
+    day3Prevbtn.addEventListener("click", function () {
+        window.location.href = "day2.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day4Prevbtn = document.getElementById('d4-prev-btn');
+    day4Prevbtn.addEventListener("click", function () {
+        window.location.href = "day3.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day5Prevbtn = document.getElementById('d5-prev-btn');
+    day5Prevbtn.addEventListener("click", function () {
+        window.location.href = "day4.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day6Prevbtn = document.getElementById('d6-prev-btn');
+    day6Prevbtn.addEventListener("click", function () {
+        window.location.href = "day5.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day7Prevbtn = document.getElementById('d7-prev-btn');
+    day7Prevbtn.addEventListener("click", function () {
+        window.location.href = "day6.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day8Prevbtn = document.getElementById('d8-prev-btn');
+    day8Prevbtn.addEventListener("click", function () {
+        window.location.href = "day7.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day9Prevbtn = document.getElementById('d9-prev-btn');
+    day9Prevbtn.addEventListener("click", function () {
+        window.location.href = "day8.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day10Prevbtn = document.getElementById('d10-prev-btn');
+    day10Prevbtn.addEventListener("click", function () {
+        window.location.href = "day9.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const day11Prevbtn = document.getElementById('d11-prev-btn');
+    day11Prevbtn.addEventListener("click", function () {
+        window.location.href = "day10.html";
+    });
+});
